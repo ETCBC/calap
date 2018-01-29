17b57908837bdf0feac1f24df519e8cd0caf371f
Dirk Roorda <dirk.roorda@dans.knaw.nl>
2018-01-29 20:28:06 +0100

    conversion to TF

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# calap
-The results of the CALAP project. Partial Peshitta data text-fabric format, converted from emdros/mql
+[![etcbc](programs/images/etcbc.png)](http://www.etcbc.nl)
+![tf](programs/images/tf-small.png)
+![psh](programs/images/peshitta.png)
+
+The results of the CALAP project.
+Partial Peshitta data text-fabric format, converted from emdros/mql
+
+More info: start in [tfFromMql.ipynb](programs/tfFromMql.ipynb)
---
+++ tf/2014/otype.tf
@@ -0,0 +1,24 @@
+@node
+@author=Eep Talstra Centre for Bible and Computer
+@converter=Dirk Roorda (TF)
+@dataset=CALAP
+@datasetName=Computer-Assisted Linguistic Analysis of the Peshitta
+@encoders=Janet Dyk, Percy van Keulen, Wido van Peursen, Constantijn Sikkel, HendrikJan Bosman, Konrad Jenner, Eep Talstra, Dirk Bakker, Jeffrey A. Volkmer, Ariel Gutman
+@iso639=syc
+@language=Syriac
+@organizations=Peshitta Institute Leiden; Werkgroep Informatica at the Vrije Universiteit Amsterdam (WIVU)
+@project=CALAP and TURGAMA
+@projectPeriod=1999-2005, 2005-2010
+@projectUrl=http://www.nwo.nl/onderzoek-en-resultaten/onderzoeksprojecten/78/1900123778.html
+@valueType=str
+@version=2014
+@writtenBy=Text-Fabric
+@dateWritten=2018-01-29T19:17:25Z
+
+1-53920	word
+53921-53923	book
+53924-54020	chapter
+54021-65431	clause_atom
+65432-100326	phrase
+100327-141711	phrase_atom
+141712-144485	verse
